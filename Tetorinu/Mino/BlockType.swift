0aa31d80033300f1c6a7b5130c559da9787e4c37
me990928 <me990928@gmail.com>
2024-09-23 11:59:58 +0900

    add block type

---
+++ Tetorinu/Mino/BlockType.swift
@@ -0,0 +1,14 @@
+//
+//  BlockType.swift
+//  Tetorinu
+//
+//  Created by 広瀬友哉 on 2024/09/23.
+//
+
+enum BlockType: Int {
+    case BlockNone = 0
+    case BlockHard = 1
+    case BlockSoft = 2
+    case BlockFall = 3
+    case BlockMax = 4
+}
