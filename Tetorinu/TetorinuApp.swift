4a2ae9315a997e2f197f29cb5fb816c502c346b9
me990928 <me990928@gmail.com>
2024-09-23 13:30:10 +0900

    add fieldview template

--- Tetorinu/TetorinuApp.swift
+++ Tetorinu/TetorinuApp.swift
@@ -25,7 +25,7 @@ struct TetorinuApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TetorinuField()
         }
         .modelContainer(sharedModelContainer)
     }
