827ff1264dbaf0924d38150bb71d53d0d069997a
me990928 <me990928@gmail.com>
2024-09-23 11:55:32 +0900

    add blockmino

--- Tetorinu/Mino/Block.swift
+++ Tetorinu/Mino/Block.swift
@@ -5,3 +5,85 @@
 //  Created by 広瀬友哉 on 2024/09/23.
 //
 
+import SwiftUICore
+
+struct Block {
+    let BlockShapeI: BlockShape = .init(size: 3, pattern: [
+        [false, true, false],
+        [false, true, false],
+        [false, true, false]
+    ], color: Color.cyan)
+    
+    let BlockShapeJ: BlockShape = .init(size: 3, pattern: [
+        [false, true, false],
+        [false, true, false],
+        [true, true, false]
+    ], color: Color.blue)
+    
+    let BlockShapeL: BlockShape = .init(size: 3, pattern: [
+        [false, true, false],
+        [false, true, false],
+        [false, true, true]
+    ], color: Color.orange)
+    
+    let BlockShapeO: BlockShape = .init(size: 3, pattern: [
+        [false, true, true],
+        [false, true, true],
+        [false, false, false]
+    ], color: Color.yellow)
+    
+    let BlockShapeS: BlockShape = .init(size: 3, pattern: [
+        [false, false, true],
+        [false, true, true],
+        [false, true, false]
+    ], color: Color.red)
+    
+    let BlockShapeS2: BlockShape = .init(size: 3, pattern: [
+        [false, true, false],
+        [false, true, true],
+        [false, false, true]
+    ], color: Color.green)
+    
+    let BlockShapeT: BlockShape = .init(size: 3, pattern: [
+        [false, true, false],
+        [false, true, true],
+        [false, true, false]
+    ], color: Color.purple)
+    
+    let BlockShapeMax: Int = 7
+    
+    var shape: BlockShape
+    
+    init(shape: BlockShape, num: Int) {
+        self.shape = shape
+    }
+    
+    func getShape() -> BlockShape {
+        return shape
+    }
+    
+    func getRandomShape() -> BlockShape {
+        var shpe: BlockShape
+        
+        switch Int.random(in: 0..<BlockShapeMax) {
+        case 0:
+            shpe = BlockShapeI
+        case 1:
+            shpe = BlockShapeJ
+        case 2:
+            shpe = BlockShapeL
+        case 3:
+            shpe = BlockShapeO
+        case 4:
+            shpe = BlockShapeS
+        case 5:
+            shpe = BlockShapeS2
+        case 6:
+            shpe = BlockShapeT
+        default:
+            shpe = BlockShapeI
+        }
+        
+        return shpe
+    }
+}
