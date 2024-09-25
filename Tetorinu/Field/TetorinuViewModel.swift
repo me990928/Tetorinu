ee9f234858afdbed77b8d672081a0a42973d7b01
me990928 <me990928@gmail.com>
2024-09-25 10:38:09 +0900

    テトリス本体

---
+++ Tetorinu/Field/TetorinuViewModel.swift
@@ -0,0 +1,214 @@
+//
+//  TetorinuViewModel.swift
+//  Tetorinu
+//
+//  Created by 広瀬友哉 on 2024/09/23.
+//
+
+import Observation
+import SwiftUICore
+import Combine
+
+@Observable
+class TetorinuViewModel {
+    let defaultField: [[Field]] = FieldTemplate.init().defaultField
+    let blockSizeMax: Int = Block.init().BlockShapeNull.getSize()
+    let fieldWidth: Int = 10
+    let fieldHeight: Int = 20
+    
+    var isRunning: Bool = false
+    
+    var field: [[Field]]
+    
+    var outputField: [[Field]]
+    
+    var downBlock: DownBlock = .init()
+    
+    init() {
+        self.field = defaultField
+        self.outputField = defaultField
+    }
+    
+    func initTetorinu(){
+        field = defaultField
+        drawScreen()
+    }
+    
+    func initBlock(){
+        downBlock.shape = Block.init().getRandomShape()
+        
+        let rotateCount: Int = Int.random(in: 0..<4)
+        for _ in 0..<rotateCount{
+            rotateBlock()
+        }
+        
+        downBlock.x = fieldWidth / 2 - Block.init().BlockShapeNull.getSize() / 2
+        downBlock.y = 0
+    }
+    
+    func rotateBlock(){
+        var rotateBlockDownBlock: DownBlock = DownBlock()
+        rotateBlockDownBlock.shape = Block.init().getShape()
+        
+        let size: Int = downBlock.shape.getSize()
+        
+        var newPattern: [[Bool]] = Block.init().BlockShapeNull.getPattern()
+        
+        // 90度回転
+        for y in 0..<size{
+            for x in 0..<size{
+                newPattern[size - 1 - x][y] = downBlock.shape.getPattern()[y][x]
+            }
+        }
+        
+        rotateBlockDownBlock.shape.setPattern(newPattern)
+        let dx = downBlock.x
+        let dy = downBlock.y
+        
+        rotateBlockDownBlock.x = dx
+        rotateBlockDownBlock.y = dy
+        
+        for y in 0..<size{
+            for x in 0..<size{
+                if rotateBlockDownBlock.y + y >= fieldHeight || rotateBlockDownBlock.x + x >= fieldWidth {
+                    return
+                }
+                if rotateBlockDownBlock.shape.getPattern()[y][x] && field[rotateBlockDownBlock.y + y][rotateBlockDownBlock.x + x].type == BlockType.BlockHard.rawValue {
+                    return
+                }
+            }
+        }
+        
+        downBlock.shape.setPattern(newPattern)
+        
+    }
+    
+    func fallBlock(){
+        let lastBlock: DownBlock = DownBlock(block: downBlock)
+        downBlock.y += 1
+        
+        if blockIntersectField {
+            downBlock = DownBlock(block: lastBlock)
+            for y in 0..<blockSizeMax{
+                for x in 0..<blockSizeMax{
+                    if downBlock.shape.getPattern()[y][x] {
+                        field[downBlock.y + y][downBlock.x + x].type = BlockType.BlockSoft.rawValue
+                        field[downBlock.y + y][downBlock.x + x].color = downBlock.shape.getColor()
+                    }
+                }
+            }
+            eraseLine()
+            initBlock()
+        }
+        
+        if blockIntersectField {
+            // game over
+            initTetorinu()
+        }
+        
+        drawScreen()
+    }
+    
+    func eraseLine(){
+        for y in 0..<fieldHeight {
+            var completeLine: Bool = true
+            
+            // yの列が揃ったか確認
+            for x in 0..<fieldWidth{
+                if field[y][x].type == BlockType.BlockNone.rawValue {
+                    completeLine = false
+                    break
+                }
+            }
+            
+            // 揃ったら
+            if completeLine {
+                
+                // y列を消す
+                for x in 0..<fieldWidth{
+                    if field[y][x].type == BlockType.BlockSoft.rawValue {
+                        field[y][x].type = BlockType.BlockNone.rawValue
+                    }
+                }
+                
+                
+                for x in 0..<fieldWidth{
+                    for y2 in stride(from: y, through: 0, by: -1) {
+                        if field[y2][x].type == BlockType.BlockHard.rawValue {
+                            break
+                        }
+                        
+                        if y2 == 0 {
+                            field[y2][x].type = BlockType.BlockNone.rawValue
+                            field[y2][x].color = .clear
+                        } else {
+                            if (field[y2 - 1][x].type != BlockType.BlockHard.rawValue) {
+                                field[y2][x] = field[y2 - 1][x]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    var blockIntersectField: Bool{
+        for y in 0..<downBlock.shape.getSize() {
+            for x in 0..<downBlock.shape.getSize() {
+                if downBlock.shape.getPattern()[y][x] {
+                    
+                    let globalX: Int = downBlock.x + x
+                    let globalY: Int = downBlock.y + y
+                    
+                    if (globalX < 0) || (globalX >= fieldWidth) || (globalY < 0) || (globalY >= fieldHeight) || (field[globalY][globalX].type != BlockType.BlockNone.rawValue) {
+                        return true
+                    }
+                    
+                }
+            }
+        }
+        return false
+    }
+    
+    func drawScreen(){
+        var screen: [[Field]] = field
+        
+        for y in 0..<blockSizeMax{
+            for x in 0..<blockSizeMax{
+                if downBlock.shape.getPattern()[y][x] {
+                    screen[downBlock.y + y][downBlock.x + x].type = BlockType.BlockFall.rawValue
+                    screen[downBlock.y + y][downBlock.x + x].color = downBlock.shape.getColor()
+                }
+            }
+        }
+        outputField = screen
+    }
+   
+    func minoControl(command: Command){
+        let lastBlock = downBlock
+        
+        switch command {
+        case .left:
+            downBlock.x -= 1
+        case .right:
+            downBlock.x += 1
+        case .forward:
+            downBlock.y += 1
+        case .rotate:
+            rotateBlock()
+        }
+        
+        if blockIntersectField {
+            downBlock = lastBlock
+        } else {
+            drawScreen()
+        }
+    }
+}
+
+enum Command: String {
+    case left = "a"
+    case right = "d"
+    case forward = "w"
+    case rotate = "s"
+}
