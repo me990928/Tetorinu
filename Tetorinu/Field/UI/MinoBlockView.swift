54e53ac1d6dc69753573349fb10407b9ef2d51a7
me990928 <me990928@gmail.com>
2024-09-27 04:13:17 +0900

    テンプレートの修正

---
+++ Tetorinu/Field/UI/MinoBlockView.swift
@@ -0,0 +1,18 @@
+//
+//  MinoBlockView.swift
+//  Tetorinu
+//
+//  Created by 広瀬友哉 on 2024/09/27.
+//
+
+import SwiftUI
+
+struct MinoBlockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MinoBlockView()
+}
---
+++ Tetorinu/Field/UI/ResumeView.swift
@@ -0,0 +1,18 @@
+//
+//  ResumeView.swift
+//  Tetorinu
+//
+//  Created by 広瀬友哉 on 2024/09/27.
+//
+
+import SwiftUI
+
+struct ResumeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResumeView()
+}
