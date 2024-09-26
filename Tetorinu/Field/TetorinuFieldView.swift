d48028a348c8da61966e4ea8c0826d61425c9b61
me990928 <me990928@gmail.com>
2024-09-26 11:47:52 +0900

    レイアウト変更_コントローラ

--- Tetorinu/Field/TetorinuFieldView.swift
+++ Tetorinu/Field/TetorinuFieldView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct TetorinuFieldView: View {
     
     @State var tetorinuVM: TetorinuViewModel = .init()
+    @StateObject var deviceOrientation: DeviceOrientation = DeviceOrientation()
     
     @State var width: CGFloat = 0
     @State var height: CGFloat = 0
@@ -21,12 +23,15 @@ struct TetorinuFieldView: View {
     
     @State var timer: Timer?
     
+    @State private var lastUpdate: Date = Date()
+    @State private var updateInterval: TimeInterval = 0.1 // 0.5秒ごとに発火
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 VStack{
                     Spacer()
-                    HStack{
+                    HStack(spacing: 0){
                         Spacer()
                         VStack(spacing: 0){
                             ForEach(0..<tetorinuVM.fieldHeight, id: \.self) { y in
@@ -49,7 +54,9 @@ struct TetorinuFieldView: View {
                                         if tetorinuVM.outputField[y][x].type == BlockType.BlockFall.rawValue {
                                             Rectangle()
                                                 .fill(tetorinuVM.outputField[y][x].color)
-                                                .frame(width: blockSize, height: blockSize).border(Color.black, width: 0.2)
+                                                .frame(width: blockSize, height: blockSize).border(Color.black, width: 0.2).onTapGesture {
+                                                    tetorinuVM.minoControl(command: .rotate)
+                                                }
                                         }
                                         
                                         if tetorinuVM.outputField[y][x].type == BlockType.BlockSoft.rawValue {
@@ -59,32 +66,45 @@ struct TetorinuFieldView: View {
                                         }
                                     }
                                     Spacer()
-                                }.onTapGesture {
                                 }
                             }.onAppear() {
-                                self.width = geometry.size.height * 0.4
-                                self.height = self.width * 2
-                                self.blockSize = self.width / 10
-                                //
+                                
+                                UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+                                
+                                if deviceOrientation.orientation.isPortrait {
+                                    self.width = geometry.size.height * 0.4
+                                    self.height = self.width * 2
+                                    self.blockSize = self.width / 10
+                                } else {
+                                    self.width = geometry.size.height * 0.4
+                                    self.height = self.width * 2
+                                    self.blockSize = self.width / 10
+                                }
+                                
+                                // セットアップ
                                 tetorinuVM.initTetorinu()
                                 tetorinuVM.initBlock()
                                 tetorinuVM.nextBlock()
                                 tetorinuVM.drawScreen()
-                                
                                 tetorinuVM.tetorinuTimer()
                             }
-                        }
-                        Spacer()
+                            .onChange(of: deviceOrientation.orientation.isPortrait) { oldValue, newValue in
+                                
+                                self.width = geometry.size.height * 0.4
+                                self.height = self.width * 2
+                                self.blockSize = self.width / 10
+                            }
+                        }.frame(width: width)
                         VStack(spacing: 0){
                             
                             
                             Circle().fill(.blue).overlay {
-                                    Image(systemName: tetorinuVM.isRunning ? "pause.fill" : "play.fill").foregroundStyle(.white)
-                                }.padding(.bottom, 20)
+                                Image(systemName: tetorinuVM.isRunning ? "pause.fill" : "play.fill").foregroundStyle(.white)
+                            }.frame(width: width * 0.2).padding(.bottom, 20)
                                 .onTapGesture {
                                     tetorinuVM.isRunning.toggle()
                                 }
-
+                            
                             
                             HStack{
                                 Spacer()
@@ -108,44 +128,14 @@ struct TetorinuFieldView: View {
                                     }
                                 }
                             }.border(Color.white, width: 1)
+                            VStack{
+                                Text("Score").foregroundStyle(.white)
+                                Text(tetorinuVM.score.description).foregroundStyle(.white).font(.caption)
+                            }.padding(.top, 20)
                             Spacer()
-                        }.padding(.top, 50)
+                        }.frame(width: width * 0.3)
                         Spacer()
                     }
-                    
-                    HStack {
-                        Button {
-                            tetorinuVM.minoControl(command: .left)
-                        } label: {
-                            Circle().frame(width: width * 0.2).overlay {
-                                    Image(systemName: "arrowtriangle.left.fill").foregroundStyle(.white)
-                                }
-                        }
-                        VStack{
-                            Button {
-                                tetorinuVM.minoControl(command: .rotate)
-                            } label: {
-                                Circle().frame(width: width * 0.2).overlay {
-                                    Image(systemName: "arrow.triangle.2.circlepath").foregroundStyle(.white).bold().font(.title2)
-                                }
-                            }
-                            Button {
-                                tetorinuVM.minoControl(command: .forward)
-                            } label: {
-                                Circle().frame(width: width * 0.2).overlay {
-                                    Image(systemName: "arrowtriangle.down.fill").foregroundStyle(.white)
-                                }
-                            }
-                        }
-                        Button {
-                            tetorinuVM.minoControl(command: .right)
-                        } label: {
-                            Circle().frame(width: width * 0.2).overlay {
-                                Image(systemName: "arrowtriangle.right.fill").foregroundStyle(.white)
-                            }
-                        }
-                        
-                    }.padding(.vertical)
                     Spacer()
                 }.background(Color.black)
                 
@@ -156,13 +146,13 @@ struct TetorinuFieldView: View {
                     }.frame(width: geometry.size.width * 0.9, height: geometry.size.width * 0.5).background(
                         .ultraThinMaterial,
                         in: RoundedRectangle(cornerRadius: 30, style: .continuous)
-                     ).onTapGesture {
+                    ).onTapGesture {
                         tetorinuVM.isGameOver.toggle()
-                         
-                         tetorinuVM.initTetorinu()
-                         tetorinuVM.initBlock()
-                         tetorinuVM.nextBlock()
-                         tetorinuVM.drawScreen()
+                        
+                        tetorinuVM.initTetorinu()
+                        tetorinuVM.initBlock()
+                        tetorinuVM.nextBlock()
+                        tetorinuVM.drawScreen()
                     }
                 }
                 if !tetorinuVM.isRunning && !tetorinuVM.isGameOver {
@@ -171,17 +161,23 @@ struct TetorinuFieldView: View {
                     }.frame(width: geometry.size.width * 0.5, height: geometry.size.width * 0.3).background(
                         .ultraThinMaterial,
                         in: RoundedRectangle(cornerRadius: 30, style: .continuous)
-                     ).onTapGesture {
+                    ).onTapGesture {
                         tetorinuVM.isRunning.toggle()
                     }
                 }
             }
+            TetorinuController(tetorinuVM: tetorinuVM)
         }.onAppear(){
             // 通知を監視する
             NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: .main) { _ in
                 tetorinuVM.isRunning = false
                 print("App will resign active (moving to background)")
             }
+            
+            UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+            
+        }.onDisappear(){
+            UIDevice.current.endGeneratingDeviceOrientationNotifications()
         }
     }
 }
---
+++ Tetorinu/Field/UI/TetorinuController.swift
@@ -0,0 +1,18 @@
+//
+//  TetorinuController.swift
+//  Tetorinu
+//
+//  Created by 広瀬友哉 on 2024/09/26.
+//
+
+import SwiftUI
+
+struct TetorinuController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TetorinuController()
+}
