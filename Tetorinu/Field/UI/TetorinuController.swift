e07ebac7cec15c142d73cb746372b80e367a51f0
me990928 <me990928@gmail.com>
2024-09-27 04:17:50 +0900

    コントローラーの改修

--- Tetorinu/Field/UI/TetorinuController.swift
+++ Tetorinu/Field/UI/TetorinuController.swift
@@ -9,37 +9,89 @@ import SwiftUI
 
 struct TetorinuController: View {
     
-    @State var tetorinuVM: TetorinuViewModel
+    @AppStorage("firstLaunch") var firstLaunch: Bool = true
+    
+    @Binding var tetorinuVM: TetorinuViewModel
     
     @State var blockSize: CGFloat = 10
+    
+    @State var valOpacity: Double = 0
     
     var body: some View {
         GeometryReader { geo in
             VStack {
                 Spacer()
                 HStack{
-                    Rectangle().fill(.red).onTapGesture {
+                    Rectangle().fill(.red).overlay(content: {
+                        VStack{
+                            if firstLaunch {
+                                Image(systemName: "arrowshape.left").font(.largeTitle).bold()
+                                
+                            }
+                        }
+                    }).onTapGesture {
                         tetorinuVM.minoControl(command: .left)
+                        let selectedFeedback =  UIImpactFeedbackGenerator(style: .heavy)
+                        selectedFeedback.impactOccurred()
                     }
                     
                     VStack {
-                        Rectangle().fill(.yellow).onTapGesture {
+                        Rectangle().fill(.yellow).overlay(content: {
+                            VStack{
+                                if firstLaunch {
+                                    Image(systemName: "arrow.counterclockwise").font(.largeTitle).bold()
+                                }
+                            }
+                        }).onTapGesture {
                             tetorinuVM.minoControl(command: .rotate)
+                            let selectedFeedback =  UIImpactFeedbackGenerator(style: .heavy)
+                            selectedFeedback.impactOccurred()
+                            
                         }
-                        Rectangle().fill(.green).onTapGesture {
+                        Rectangle().fill(.green).overlay(content: {
+                            VStack {
+                                if firstLaunch {
+                                    Image(systemName: "arrowshape.down").font(.largeTitle).bold()
+                                }
+                            }
+                        }).onTapGesture {
                             tetorinuVM.minoControl(command: .forward)
+                            let selectedFeedback =  UIImpactFeedbackGenerator(style: .heavy)
+                            selectedFeedback.impactOccurred()
                         }
                     }
                     
-                    Rectangle().fill(.blue).onTapGesture {
+                    Rectangle().fill(.blue).overlay(content: {
+                        VStack{
+                            if firstLaunch {
+                                Image(systemName: "arrowshape.right").font(.largeTitle).bold()
+                            }
+                        }
+                    }).onTapGesture {
                         tetorinuVM.minoControl(command: .right)
+                        let selectedFeedback =  UIImpactFeedbackGenerator(style: .heavy)
+                        selectedFeedback.impactOccurred()
                     }
                 }.frame(height: geo.size.height / 3)
-            }.opacity(0.12)
+            }.opacity(valOpacity)
+                .onAppear() {
+                    
+                    
+                    if firstLaunch {
+                        self.valOpacity = 1
+                    } else {
+                        self.valOpacity = 0.2
+                    }
+                    
+                }.onChange(of: firstLaunch) {
+                    withAnimation {
+                        valOpacity = 0.2
+                    }
+                }
         }
     }
 }
 
-#Preview {
-    TetorinuController(tetorinuVM: TetorinuViewModel.init())
-}
+//#Preview {
+//    TetorinuController(tetorinuVM: TetorinuViewModel.init())
+//}
