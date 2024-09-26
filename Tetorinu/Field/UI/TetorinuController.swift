7162d1450f9ce2cfa4df8f597d017ee8577bc0e4
me990928 <me990928@gmail.com>
2024-09-26 11:48:37 +0900

    コントローラの変更

--- Tetorinu/Field/UI/TetorinuController.swift
+++ Tetorinu/Field/UI/TetorinuController.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct TetorinuController: View {
+    
+    @State var tetorinuVM: TetorinuViewModel
+    
+    @State var blockSize: CGFloat = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                HStack{
+                    Rectangle().fill(.red).onTapGesture {
+                        tetorinuVM.minoControl(command: .left)
+                    }
+                    
+                    VStack {
+                        Rectangle().fill(.yellow).onTapGesture {
+                            tetorinuVM.minoControl(command: .rotate)
+                        }
+                        Rectangle().fill(.green).onTapGesture {
+                            tetorinuVM.minoControl(command: .forward)
+                        }
+                    }
+                    
+                    Rectangle().fill(.blue).onTapGesture {
+                        tetorinuVM.minoControl(command: .right)
+                    }
+                }.frame(height: geo.size.height / 3)
+            }.opacity(0.12)
+        }
     }
 }
 
 #Preview {
-    TetorinuController()
+    TetorinuController(tetorinuVM: TetorinuViewModel.init())
 }
